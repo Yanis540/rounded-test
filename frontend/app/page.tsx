12ae066f9e63a0c6d7ee9501cc5fe12897bed16d
Yanis540 <yanistabellout4@gmail.com>
2024-04-11 02:44:28 +0200

    initial commit

---
+++ frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex flex-1 h-screen bg-background">
+      <h1 className="text-foreground">hi</h1>
+    </main>
+  );
+}
